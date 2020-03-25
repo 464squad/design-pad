3bacae94c3acf5eac9aaf1f6e736f513018d4b93
CodingWithCDJE <codingwithcdje@gmail.com>
2020-03-24 20:25:36 -0400

    Made the container to hold all the features and folder for placement of each component

---
+++ src/Components/DesignContainer/DesignContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class DesignContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <header>Design Pad</header>
+      </div>
+    );
+  }
+}
+
+export default DesignContainer;
