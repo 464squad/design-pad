99ef901603ead4903c1066aecedfe3be2c7cda2e
AboubakariSoumanouP <soumabakari@gmail.com>
2020-03-26 15:19:56 -0500

    create edit button

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -2,10 +2,20 @@ import React from "react";
 import PadInput from "./PadInput";
 
 class DesignContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {editing: false}
+  }
+  updateEditState =(event)=>{
+    this.setState({editing:!this.state.editing})
+    
+  }
   render() {
+    const btnClass = this.state.editing ? "editButtonActive" : "editButton";
     return (
       <div>
         <PadInput />
+        <button onClick= {this.updateEditState} className = {btnClass}>Edit</button>
       </div>
     );
   }
