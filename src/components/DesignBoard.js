2ed1a96c5b52d068ae15fda3288030bdf6f8f598
miker179 <mike.rivera179@gmail.com>
2020-03-27 17:59:11 -0400

    the layout is completed for the input and designboard

--- src/components/DesignBoard.js
+++ src/components/DesignBoard.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../designboard.css'
 
 class DesignBoard extends React.Component {
 
     render() {
 
         return(
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
+            <div className="boardContainer clearfix">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+            </div>
         );
     }
 
--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PadInput from './PadInput';
+import DesignBoard from './DesignBoard';
+import '../designboard.css';
 
 class DesignContainer extends React.Component{
     render() {
 
         return(
-            <div>
+            <div className = "masterContainer">
                 <h1>Design Pad</h1>
-                <PadInput />
+                    <PadInput />
+                    <DesignBoard />
             </div>
         );
     }
