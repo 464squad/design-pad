80bd24f54203e96f4c73af585a85303044ae2cc3
Jeson Rosario <jesonrosario22@gmail.com>
2020-03-29 18:01:51 -0400

    Moved everything back to design container, board will now hold pad

--- src/components/DesignBoard.js
+++ src/components/DesignBoard.js
@@ -4,23 +4,12 @@ import PadInput from "./PadInput";
 import "../css/DesignBoard.css";
 
 class DesignBoard extends React.Component {
-  constructor() {
-    super();
-    this.state = { editing: false };
-  }
-
-  updateEditState = event => {
-    this.setState({ editing: !this.state.editing });
-  };
-
   render() {
-    const btnClass = this.state.editing ? "editButtonActive" : "editButton";
     return (
       <div>
-        <button className={btnClass} onClick={this.updateEditState}>
-          Edit
-        </button>
-        <PadInput />
+        <DesignPad />
+        <DesignPad />
+        <DesignPad />
         <DesignPad />
       </div>
     );
--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -1,12 +1,26 @@
 import React from "react";
 import DesignBoard from "./DesignBoard";
-// import PadInput from "./PadInput";
-// import DesignPad from "./DesignPad";
+import PadInput from "./PadInput";
+import "../css/DesignContainer.css";
 
 class DesignContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = { editing: false };
+  }
+
+  updateEditState = event => {
+    this.setState({ editing: !this.state.editing });
+  };
+
   render() {
+    const btnClass = this.state.editing ? "editButtonActive" : "editButton";
     return (
       <div>
+        <button className={btnClass} onClick={this.updateEditState}>
+          Edit
+        </button>
+        <PadInput />
         <DesignBoard />
       </div>
     );
