7ba576b9ea9012f5e8f0709bca86a4285e615907
Jeson Rosario <jesonrosario22@gmail.com>
2020-03-29 16:09:50 -0400

    Moved button to design board

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -4,20 +4,9 @@ import DesignBoard from "./DesignBoard";
 // import DesignPad from "./DesignPad";
 
 class DesignContainer extends React.Component {
-  constructor() {
-    super();
-    this.state = { editing: false };
-  }
-  updateEditState = event => {
-    this.setState({ editing: !this.state.editing });
-  };
   render() {
-    const btnClass = this.state.editing ? "editButtonActive" : "editButton";
     return (
       <div>
-        <button onClick={this.updateEditState} className={btnClass}>
-          Edit
-        </button>
         <DesignBoard />
       </div>
     );
