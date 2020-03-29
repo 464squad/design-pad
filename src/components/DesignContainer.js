073596545978ca2c47ccd1cfb7a8cb7800311438
Jeson Rosario <jesonrosario22@gmail.com>
2020-03-29 12:56:29 -0400

    Empty spaces removed

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -4,18 +4,19 @@ import PadInput from "./PadInput";
 class DesignContainer extends React.Component {
   constructor() {
     super();
-    this.state = {editing: false}
+    this.state = { editing: false };
   }
-  updateEditState =(event)=>{
-    this.setState({editing:!this.state.editing})
-    
-  }
+  updateEditState = event => {
+    this.setState({ editing: !this.state.editing });
+  };
   render() {
     const btnClass = this.state.editing ? "editButtonActive" : "editButton";
     return (
       <div>
         <PadInput />
-        <button onClick= {this.updateEditState} className = {btnClass}>Edit</button>
+        <button onClick={this.updateEditState} className={btnClass}>
+          Edit
+        </button>
       </div>
     );
   }
