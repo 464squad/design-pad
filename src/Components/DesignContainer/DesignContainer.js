78b6f223398f2c91d4b269e8ab72d18766ab2ee2
Christopher Estevez <CodingWithCDJE@gmail.com>
2020-03-28 23:34:31 -0400

    imported missing code from master

--- src/Components/DesignContainer/DesignContainer.js
+++ src/Components/DesignContainer/DesignContainer.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import DesignPanel from '../DesignPanel/DesignPanel';
-=======
 /*
 import Controller from './Controller.js';
 import Greyboxes from './Greyboxes.js'
@@ -10,22 +8,13 @@ import styles from './DesignContainer.css';
 */
 import './DesignContainer.css';
 
->>>>>>> 4f6d6642e41d1602eaf41cf59f664923cf1dd5f8
-
 class DesignContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
       editActive: false
     };
-=======
-
-    }
->>>>>>> 4f6d6642e41d1602eaf41cf59f664923cf1dd5f8
   }
- 
-  
 
   updateEditButton = () => {
     const { editActive } = this.state;
@@ -48,28 +37,19 @@ class DesignContainer extends Component {
          
          </div>
        
-       */ 
-  
+       */
 
   render() {
     const { editActive } = this.state;
 
     return (
-
       <div className='DesignContainer__container'>
-      <header className='DesignContainer__header'>Design Pad</header>
-      <button onClick={this.updateEditButton}>Edit</button>
-      {editActive === false ? '' : <DesignPanel />}
-    </div>
-
-
-
-
-
-    )
+        <header className='DesignContainer__header'>Design Pad</header>
+        <button onClick={this.updateEditButton}>Edit</button>
+        {editActive === false ? '' : <DesignPanel />}
+      </div>
+    );
   }
 }
-
-
 
 export default DesignContainer;
