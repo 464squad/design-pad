57eab00b13007bb75964e7fb78068c914d70560a
Christopher Estevez <CodingWithCDJE@gmail.com>
2020-03-28 23:09:24 -0400

    Panel changes based on the edit button

--- src/Components/DesignContainer/DesignContainer.js
+++ src/Components/DesignContainer/DesignContainer.js
@@ -4,14 +4,26 @@ import DesignPanel from '../DesignPanel/DesignPanel';
 class DesignContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      editActive: false
+    };
   }
 
+  updateEditButton = () => {
+    const { editActive } = this.state;
+    editActive === false
+      ? this.setState({ editActive: true })
+      : this.setState({ editActive: false });
+  };
+
   render() {
+    const { editActive } = this.state;
+
     return (
       <div>
         <header>Design Pad</header>
-        <DesignPanel />
+        <button onClick={this.updateEditButton}>Edit</button>
+        {editActive === false ? '' : <DesignPanel />}
       </div>
     );
   }
--- src/Components/DesignPanel/DesignPanel.js
+++ src/Components/DesignPanel/DesignPanel.js
@@ -6,28 +6,28 @@ const handleDrag = (e, color) => {};
 const DesignPanel = () => {
   return (
     <div className='DesignPanel__container'>
-      <button id='1' className='DesignPanel__button'>
+      <button id='1' className='DesignPanel__button' draggable='true'>
         1
       </button>
-      <button id='2' className='DesignPanel__button'>
+      <button id='2' className='DesignPanel__button' draggable='true'>
         2
       </button>
-      <button id='3' className='DesignPanel__button'>
+      <button id='3' className='DesignPanel__button' draggable='true'>
         3
       </button>
-      <button id='4' className='DesignPanel__button'>
+      <button id='4' className='DesignPanel__button' draggable='true'>
         4
       </button>
-      <button id='5' className='DesignPanel__button'>
+      <button id='5' className='DesignPanel__button' draggable='true'>
         5
       </button>
-      <button id='6' className='DesignPanel__button'>
+      <button id='6' className='DesignPanel__button' draggable='true'>
         6
       </button>
-      <button id='7' className='DesignPanel__button'>
+      <button id='7' className='DesignPanel__button' draggable='true'>
         7
       </button>
-      <button id='8' className='DesignPanel__button'>
+      <button id='8' className='DesignPanel__button' draggable='true'>
         8
       </button>
     </div>
