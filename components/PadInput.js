0931fad28e19e2682949013394b9ac2e09f24cc6
KabdulDev <60675774+KabdulDev@users.noreply.github.com>
2020-04-01 17:56:17 -0400

    Add files via upload

--- components/PadInput.js
+++ components/PadInput.js
@@ -5,29 +5,40 @@ class PadInput extends React.Component {
      super(props);
 
      this.state = {
-         padButton: [1, 2, 3, 4, 5, 6, 7, 8]
+         padButtons: [
+            {id:1, num: "1"}, 
+            {id:2, num: "2"}, 
+            {id:3, num: "3"}, 
+            {id:4, num: "4"}, 
+            {id:5, num: "5"}, 
+            {id:6, num: "6"}, 
+            {id:7, num: "7"}, 
+            {id:8, num: "8"}
+        ]
      }
 
 
      }
  
     render(){
-        const { padButton } = this.state;
+        const {padButtons} = this.state;
         return(
             <div className= "padButton">
-                <div
-                key={padButton.index}
-                >
-                    {padButton.index}
-                </div>
+                {
+                    padButtons.map (padButton =>
+                        <div
+                            key={padButton.id}
+                            draggable
+                        >
+                            {padButton.num}
+                        </div>
+                    )
+                }
             </div>
-
-        );
-
-
+        )
     }
-
-
+    
+    
 }
 
 export default PadInput;
