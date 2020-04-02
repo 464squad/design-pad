cac83037b73e40a161b81e2daf94619782ac9edc
Artjola Meli <artjolameli@gmail.com>
2020-04-02 17:17:34 -0400

    added the js file for the edit button

---
+++ src/components/ShowAlert.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import '../design.css'
+
+
+class ShowAlert extends Component {
+  showAlert() {
+    alert("Click okay to go back to drag/drop");
+  }
+
+  render() {
+    return <button className= "enablebutton" onClick={this.showAlert}>Edit</button>;
+  }
+}
+
+export default ShowAlert;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<ShowAlert />, rootElement);
