7d58b0a00b00e32c25d5b9a027c58064b20ef1d3
Ala123Sobhan <ala123sobhan@gmail.com>
2020-10-22 09:13:29 -0400

    adding initial setup components

---
+++ src/components/DesignBoard.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import "./style.css"
+class DesignPad extends Component{
+
+  
+    render(){
+        return(
+        <button className="square">
+             design pads
+            </button>
+        )
+    }
+}
+ class DesignBoard extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             squares: Array(4).fill(null)
+        }
+    }
+    
+    renderDesignPad(i)
+    {
+        return <DesignPad value ={this.state.squares[i]}/>
+    }
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderDesignPad(0)}
+                    {this.renderDesignPad(1)}
+
+                </div>
+                <div className="board-row">
+                {this.renderDesignPad(2)}
+                {this.renderDesignPad(3)}
+
+                </div>
+
+                
+            </div>
+        )
+    }
+}
+
+export default DesignBoard
---
+++ src/components/PadInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./style.css"
+export class PadInput extends Component {
+    render() {
+        return (
+            <div className="pad-box">
+                <div>PadInput</div>
+            </div>
+        )
+    }
+}
+
+export default PadInput
