d141941b57a43991ce55d77b506d703e767864ad
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-10-22 18:38:38 -0400

    making more changes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,14 @@ import DesignContainer from './components/DesignContainer';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
+
+
+
     <div style={{ textAlign:"center", justifyContent:"center"}} >
     <h1>DESIGN PAD</h1>
     <DesignContainer/>
 
->>>>>>> 490eae8377d087911d76ab9549d2b0079f2eae9f
+
     </div>
   );
 }
