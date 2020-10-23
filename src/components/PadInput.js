586573b35a3c4ba1f57a039b20e923284f41173f
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-10-23 17:53:27 -0400

    added css grid to padding input

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -12,29 +12,17 @@ export class PadInput extends Component {
     }
     render() {
     return (
-        <div>
-        
+        //<div class="padInput" >PadInput</div>
         <div className="pad-box">
-        <div>PadInput</div>
-        <div>
-        <div id = "clr1" style={{border: "1px solid #9999", backgroundColor: "LightPink"}} draggable={true} onDragStart={this.drag}>1</div>
-          <div id = "clr2" style={{border: "1px solid #9999", backgroundColor: "LightGreen"}} draggable={true} onDragStart={this.drag} >2</div>
-          </div>
-          <div>
-          <div id = "clr3" style={{border: "1px solid #9999", backgroundColor: "HotPink"}} draggable={true} onDragStart={this.drag}>3</div> 
-          <div id = "clr4" style={{border: "1px solid #9999", backgroundColor: "LightSalmon"}} draggable={true} onDragStart={this.drag}>4</div>
-          </div>
-          <div >
-          <div id = "clr5" style={{border: "1px solid #9999", backgroundColor: "LightSeaGreen"}} draggable={true} onDragStart={this.drag}>5</div>
-          <div id = "clr6" style={{border: "1px solid #9999",backgroundColor:"OrangeRed"}} draggable={true} onDragStart={this.drag}>6</div>
-          </div>
-          <div>
-          <div id = "clr7" style={{border: "1px solid #9999", backgroundColor:"Violet"}} draggable={true} onDragStart={this.drag}>7</div>
-          <div id = "clr8" style={{border: "1px solid #9999",backgroundColor:"yellowgreen"}}draggable={true} onDragStart={this.drag}>8</div>
-          </div>
+            <div class="pad_Item" id = "clr1" style={{border: "1px solid #9999", backgroundColor: "LightPink"}} draggable={true} onDragStart={this.drag}>1</div>
+            <div class="pad_Item" id = "clr2" style={{border: "1px solid #9999", backgroundColor: "LightGreen"}} draggable={true} onDragStart={this.drag} >2</div>
+            <div class="pad_Item" id = "clr3" style={{border: "1px solid #9999", backgroundColor: "HotPink"}} draggable={true} onDragStart={this.drag}>3</div> 
+            <div class="pad_Item" id = "clr4" style={{border: "1px solid #9999", backgroundColor: "LightSalmon"}} draggable={true} onDragStart={this.drag}>4</div>
+            <div class="pad_Item" id = "clr5" style={{border: "1px solid #9999", backgroundColor: "LightSeaGreen"}} draggable={true} onDragStart={this.drag}>5</div>
+            <div class="pad_Item" id = "clr6" style={{border: "1px solid #9999",backgroundColor:"OrangeRed"}} draggable={true} onDragStart={this.drag}>6</div>
+            <div class="pad_Item" id = "clr7" style={{border: "1px solid #9999", backgroundColor:"Violet"}} draggable={true} onDragStart={this.drag}>7</div>
+            <div class="pad_Item" id = "clr8" style={{border: "1px solid #9999",backgroundColor:"yellowgreen"}}draggable={true} onDragStart={this.drag}>8</div>
         </div>
-        </div>
-
         )
     }
 }
