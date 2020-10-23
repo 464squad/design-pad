8fd55d35d37f33e4cf010a25c67467d0510d6627
Ganesh Jainarain <RicheyJay2@gmail.com>
2020-10-23 19:23:05 -0400

    Added to App.js and DesignContainer.js(added the comps)

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -1,1 +1,34 @@
-//test
+import React from 'react';
+import DesignPad from './DesignPad';
+import DesignBoard from './DesignBoard';
+import PadInput from './PadInput';
+
+class DesignContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        
+
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <h1>DESIGN PAD</h1>
+                {/*TODO - Add PadInput Component */}
+                    <PadInput/>            
+                <br/>
+
+                {/*TODO - Add DesignBoard Component */}
+                    <DesignBoard/>
+                    
+                <br/>
+                {/*TODO - Add DesignPad Component */}
+                    <DesignPad/>
+                
+            </div>
+        );
+    }
+}
+
+export default DesignContainer;
