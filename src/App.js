12aa57ff3a44712a8590d0fdac417c33f4d29f04
Michael Nti <michaelnti@Michaels-MBP-2.home>
2020-10-24 17:42:19 -0400

    title animation added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DesignContainer from './components/DesignContainer';
 import "./components/style.css"
+import Title from './components/Title'
 function App() {
+  
   return (
     <div className="heading" >
-    <h1 className="padTitle" >D E S I G N P A D</h1>
+    <Title />
     <DesignContainer/>
 
 
