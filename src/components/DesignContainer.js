beb2112e90cc12a7ea011183d08f99d32a6264da
Ganesh Jainarain <RicheyJay2@gmail.com>
2020-10-23 22:53:26 -0400

    Added some boilerplated code so it could be easier to come up with logic, handleEditChange, editButton, and table in DesignBoard

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -6,27 +6,43 @@ import PadInput from './PadInput';
 class DesignContainer extends React.Component {
     constructor(props) {
         super(props)
-        
 
+
+    }
+
+    editButton = (event) => {
+        // to prevent page reload on form submit
+        event.preventDefault();
+
+        /*
+            TODO - Logic for calling props to handle submission and setting state changes
+        */
+        console.log(this.state)
+        this.props.onSubmit(this.state)
     }
 
     render() {
 
-        return (
-            <div className="container">
-                <h1>DESIGN PAD</h1>
-                {/*TODO - Add PadInput Component */}
-                    <PadInput/>            
-                <br/>
+        return ( <
+            div className = "container" >
+            <
+            h1 > DESIGN PAD < /h1> { /*TODO - Add PadInput Component */ } <
+            PadInput / >
+            <
+            br / >
 
-                {/*TODO - Add DesignBoard Component */}
-                    <DesignBoard/>
-                    
-                <br/>
-                {/*TODO - Add DesignPad Component */}
-                    <DesignPad/>
-                
-            </div>
+            { /*TODO - Add DesignBoard Component */ } <
+            DesignBoard / >
+
+            <
+            br / > { /*TODO - Add DesignPad Component */ } <
+            DesignPad / >
+            <
+            br / > { /*TODO - Add editButton Component */ } <
+            button onClick = { this.editButton } > EDIT < /button>
+
+            <
+            /div>
         );
     }
 }
