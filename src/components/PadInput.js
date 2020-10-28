259319b334e6e69728692f75458d4648a4db7bd5
Ganesh Jainarain <RicheyJay2@gmail.com>
2020-10-28 00:28:56 -0400

    Made changes to the html and css files

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Form extends Component {
     constructor(props) {
         super(props);
-        
+
         /*
             TODO - set initial state for editButton
 
         */
-       
+
     }
 
 
@@ -16,36 +16,37 @@ class Form extends Component {
         /*
             TODO - Logic for changing state based on Edit button click changes
         */
-       console.log("EDIT BUTTON CLICKED")
-       console.log(event.target)
-       
-       //changing the state
-       this.setState({
-        [event.target.editButton]: event.target.value}) 
-        
+        console.log("EDIT BUTTON CLICKED")
+        console.log(event.target)
+
+        //changing the state
+        this.setState({
+            [event.target.editButton]: event.target.value
+        })
+
     }
 
 
     editButton = (event) => {
         // to prevent page reload on form submit
         event.preventDefault();
-        
+
         /*
             TODO - Logic for calling props to handle submission and setting state changes
         */
         console.log(this.state)
         this.props.handleEditChange(this.state)
-    }   
+    }
 
 
     render() {
 
-        return(
+        return (null
 
-            <button onClick={this.editButton}> EDIT </button>
-           
+            //<button onClick={this.editButton}> EDIT </button>
+
         )
-    
+
     }
 }
 
