86df11508cb607709d8c39871bef145e6343ce85
chris <chris.espinal92@hotmail.com>
2020-10-31 08:22:42 -0400

    added funcionality to PadInput

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -7,13 +7,6 @@ export default function PadInput(props){
 
     return(
         <div className="PadInput">
-            {/* <PadItem edit={props.edit} color="red"/>
-            <PadItem edit={props.edit} color="blue"/>            <PadItem edit={props.edit} color="orange"/>
-            <PadItem edit={props.edit} color="yellow"/>
-            <PadItem edit={props.edit} color="green"/>
-            <PadItem edit={props.edit} color="brown"/>
-            <PadItem edit={props.edit} color="pink"/>
-            <PadItem edit={props.edit} color="purple"/> */}
             <div className="PadItem"> <PadItem edit={props.edit} color="red"/></div>
             <div className="PadItem"> <PadItem edit={props.edit} color="blue"/></div>
             <div className="PadItem"> <PadItem edit={props.edit} color="orange"/></div>
