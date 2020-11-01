d0a5babbd2f469c89dd07b067799dd177e6d1109
francisco cuesta <cuestafrancisco@gmail.com>
2020-11-01 18:04:17 -0500

    updating local repo

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -21,42 +21,22 @@ function PadInput(props) {
   
   return (
     <div className="main-container">
-      <table>
-        <tbody class= "table">
-          <tr>
-            <td class = "tableBlue">
-              <Pad editing={props.editing} color="blue" />
-            </td>
-            <td class = "tableRed">
-              <Pad editing={props.editing} color="red" />
-            </td>
-          </tr>
-          <tr>
-            <td class = "tablePurple">
-              <Pad editing={props.editing} color="purple" />
-            </td>
-            <td class = "tableGreen">
-              <Pad editing={props.editing} color="green" />
-            </td>
-          </tr>
-          <tr>
-            <td style={{ color: "black" }} class = "tablePink">
-              <Pad editing={props.editing} color="pink" />
-            </td>
-            <td style={{color: "black"}} class = "tableYellow">
-              <Pad editing={props.editing} color="yellow" />
-            </td>
-          </tr>
-          <tr>
-            <td class = "tableBrown">
-              <Pad editing={props.editing} color="brown" />
-            </td>
-            <td class = "tableOrange">
-              <Pad editing={props.editing} color="orange" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="row">
+        <Pad editing={props.editing} color="blue" />
+        <Pad editing={props.editing} color="red" />
+      </div>
+      <div className="row">
+        <Pad editing={props.editing} color="purple" />
+        <Pad editing={props.editing} color="green" />
+      </div>
+      <div className="row">
+        <Pad editing={props.editing} color="pink"  />
+        <Pad editing={props.editing} color="yellow" />
+      </div>
+      <div className="row">
+        <Pad editing={props.editing} color="black" />
+        <Pad editing={props.editing} color="orange" />
+      </div>
     </div>
   );
 }
