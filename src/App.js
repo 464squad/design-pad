3f90b407f674c0631fb9fa37d64a2f0bae3a6a21
J <Jordan.Roman90@gmail.com>
2020-11-02 16:34:13 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import PadInput from './components/PadInput';
 import DesignPad from './components/DesignPad'
 import EditBtn from './components/EditBtn'
 import { Container, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="container">  
-    <div className="row justify-content-md-center">
-      <PadInput />
-      <DesignPad />
-      </div>
-      <EditBtn />
+          <div className="row justify-content-md-center">
+            <PadInput />
+            <DesignPad />
+            </div>
+            <div className="row justify-content-md-center">
+              <EditBtn/>
+            </div> 
+            
     </div>
   );
 }
