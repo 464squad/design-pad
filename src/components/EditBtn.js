311e581c63ef086838ef5827cc375b29d8c4bd28
J <Jordan.Roman90@gmail.com>
2020-11-02 16:30:31 -0500

    edit btn functionality

--- src/components/EditBtn.js
+++ src/components/EditBtn.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { button } from 'react-bootstrap'
 
 class EditBtn extends Component {
     constructor(props) {
@@ -14,18 +14,32 @@ class EditBtn extends Component {
         this.setState(state => ({
             isToggleOn: !state.isToggleOn
         }));
+        let elements = document.getElementsByClassName("draggable")
+        if (!this.state.isToggleOn) {
+            console.log(this.state.isToggleOn+ "false")
+
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].draggable = false; elements[i].style.opacity = 0.5;
+            }
+        }
+        else {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].draggable = true; elements[i].style.opacity = 1;
+            }
+        }
     }
 
-    render() {
-        return(
-            
-                <button onClick={this.handleClick}>
-                     {this.state.isToggleOn ? 'EDIT OFF' : 'EDIT ON'}
-                 </button>
-            
-        )
-    }
- }
 
-
- export default EditBtn
+   render() {
+       return(
+           
+               <button onClick={this.handleClick} className={this.state.isToggleOn ? 'btn btn-secondary btn-lg btn-block' : 'btn btn-primary btn-lg btn-block'}>
+                    {this.state.isToggleOn ? 'EDIT OFF' : 'EDIT ON'}
+                </button>
+           
+       )
+   }
+}
+// A lot of this code taken from official react tutorial
+// https://reactjs.org/docs/handling-events.html
+export default EditBtn
