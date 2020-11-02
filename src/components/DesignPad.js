ecb48c9541666f0b80532ef4acb0b6ed8969f060
agyapongeli77 <agyapongeli77@gmail.com>
2020-11-02 16:41:10 -0500

    Drag and drop functionality applied to the 1st of four design pad containers

--- src/components/DesignPad.js
+++ src/components/DesignPad.js
@@ -3,14 +3,33 @@ import '../styles.css'
 
 export default function DesignPad(props) {
 
+    function drop_handler(ev) {
+        console.log("Dropped");
+        ev.preventDefault();
+
+        // Get the data, which is the background color of the drop target
+        var data = ev.dataTransfer.getData("text");
+        //console.log('data being received is --->'+ data);
+
+        //cset the drop target's background color to the color passed from the PadInput;
+        ev.target.style.backgroundColor = data;
+
+        // Clear the drag data cache (for all formats/types)
+        ev.dataTransfer.clearData();
+       }
+
+       function allowDrop (ev) {
+        ev.preventDefault();
+     }
+
     return (
         <div>
-        <h1 className = "DesignPad-h1">DESIGN PAD</h1>
-            <div className="DesignPad-container">
-                <div className="DesignPad-box1"></div>
-                <div className="DesignPad-box2"></div>
-                <div className="DesignPad-box3"></div>
-                <div className="DesignPad-box4"></div>
+        <h1 className = "DesignBoard-h1">DESIGN PAD</h1>
+            <div className="DesignBoard-container">
+                <div id="designBoard_box1" className="DesignBoard-box1" style = {{backgroundColor:""}} onDrop={drop_handler} onDragOver={allowDrop}></div>
+                <div id="designBoard_box2" className="DesignBoard-box2"></div>
+                <div id="designBoard_box3" className="DesignBoard-box3"></div>
+                <div id="designBoard_box4" className="DesignBoard-box4"></div>
             </div>
         </div>
     )
--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -4,21 +4,30 @@ import '../styles.css'
 export default function PadInput(props) {
     
     //Hides the PadInput when the edit mode is on
-    let customStyle = {display:""};
-    props.editMode ? customStyle.display="grid" : customStyle.display="none";
+    //let customStyle = {display:""};
+   // props.editMode ? customStyle.display="grid" : customStyle.display="none";
+
+    
+    function dragStartHandler(event) {
+        //console.log("dragStart");
+        //console.log(event.target);
+
+        // Setting the drag's format and data passing in the event target's background color as the data 
+        event.dataTransfer.setData("text", event.target.style.backgroundColor);
+    }
 
     return (
         <div>
-        <h1 className = "PadInput-h1">Design Input</h1>
-            <div className = "PadInput-container" style = {customStyle}>
-                <div className = "PadInput-box1">1</div>
-                <div className = "PadInput-box2">2</div>
-                <div className = "PadInput-box3">3</div>
-                <div className = "PadInput-box4">4</div>
-                <div className = "PadInput-box5">5</div>
-                <div className = "PadInput-box6">6</div>
-                <div className = "PadInput-box7">7</div>
-                <div className = "PadInput-box8">8</div>
+        <h1 id = "PadInput-h1">Design Input</h1>
+            <div className = "PadInput-container">
+                <div id = "pad_Box1" className = "PadInput-box1" style = {{backgroundColor: "yellow"}} draggable={props.editMode} onDragStart={dragStartHandler}>1 </div>
+                <div id = "pad_Box2" className = "PadInput-box2" style = {{backgroundColor: "blue"}} draggable={props.editMode} onDragStart={dragStartHandler}>2</div>
+                <div id = "pad_Box3" className = "PadInput-box3" style = {{backgroundColor: "green"}} draggable={props.editMode} onDragStart={dragStartHandler}>3</div>
+                <div id = "pad_Box4" className = "PadInput-box4" style = {{backgroundColor: "red"}} draggable={props.editMode} onDragStart={dragStartHandler}>4</div>
+                <div id = "pad_Box5" className = "PadInput-box5" style = {{backgroundColor: "pink"}} draggable={props.editMode} onDragStart={dragStartHandler}>5</div>
+                <div id = "pad_Box6" className = "PadInput-box6" style = {{backgroundColor: "indigo"}} draggable={props.editMode} onDragStart={dragStartHandler}>6</div>
+                <div id = "pad_Box7" className = "PadInput-box7" style = {{backgroundColor: "white"}} draggable={props.editMode} onDragStart={dragStartHandler}>7</div>
+                <div id = "pad_Box8" className = "PadInput-box8" style = {{backgroundColor: "black"}} draggable={props.editMode} onDragStart={dragStartHandler}>8</div>
             </div>
         </div>
     )
