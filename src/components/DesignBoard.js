a4f1d3dc62993d366a5da281f939077ad4c27415
Angeline <angelineinfante892@gmail.com>
2020-11-01 23:16:09 -0500

    test

---
+++ src/components/DesignBoard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function DesignBoard(){
+    return(
+        <div>
+        
+        </div>
+
+    );
+
+ 
+    
+
+
+}
+
+export default DesignBoard;
+
+
+
---
+++ src/components/PadInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+function PadInput(){
+    return(
+        <div>
+            <ul>
+                <li>red </li>
+                <li>blue </li>
+                <li>yellow </li>
+                <li>green</li>
+                <li>pink </li>
+                <li>orange</li>
+                <li>white </li>
+                <li>black </li>
+            </ul>
+        </div>
+
+    );
+
+ 
+    
+
+
+}
+
+export default PadInput;
