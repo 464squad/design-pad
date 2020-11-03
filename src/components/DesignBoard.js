bf6c81405ca7f75ac1cb4f43f488bd67c57b45bb
Ganesh Jainarain <RicheyJay2@gmail.com>
2020-11-02 21:43:09 -0500

    latest

--- src/components/DesignBoard.js
+++ src/components/DesignBoard.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 const DesignBoardBody = () => {
     // boilerplate design board body functional component
-    return ( 
-        <div>
-            <h1 class="header"> DESIGN PAD </h1> 
-            <div class="board_container">
-                <div class="box" draggable="true" ondragstart="drag(event)"> Pad 1 </div>
-                <div class="box" draggable="true" ondragstart="drag(event)"> Pad 2 </div>
-                <div class="box" draggable="true" ondragstart="drag(event)"> Pad 3 </div>
-                <div class="box" draggable="true" ondragstart="drag(event)"> Pad 4 </div>
-            </div>
-        </div>
+    return ( <
+        div >
+        <
+        h1 class = "header" > DESIGN PAD < /h1>  <
+        div class = "board_container" >
+        <
+        div class = "box"
+        draggable = "true"
+        ondragstart = "drag(event)" > Pad 1 < /div> <
+        div class = "box"
+        draggable = "true"
+        ondragstart = "drag(event)" > Pad 2 < /div> <
+        div class = "box"
+        draggable = "true"
+        ondragstart = "drag(event)" > Pad 3 < /div> <
+        div class = "box" > Pad 4 < /div> <
+        /div> <
+        /div>
     );
 }
 
@@ -22,9 +30,12 @@ const DesignBoard = (props) => {
     //{ /*TODO - return <table> component, TableHeader and TableBody  */ }
     return (
 
-        <div>
-            <DesignBoardBody/>
-        </div> 
+        <
+        div >
+        <
+        DesignBoardBody / >
+        <
+        /div> 
 
     )
 }
