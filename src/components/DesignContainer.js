e64f83320ac701dc482233782e99f1cf61a76d14
agyapongeli77 <agyapongeli77@gmail.com>
2020-11-03 01:45:49 -0500

    additional feature added: reset button added to reset the design board when clicked

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -3,15 +3,21 @@ import DesignBoard from './DesignBoard'
 import PadInput from './PadInput'
 
 export default function DesignContainer() {
-    
+
     const [isEdittable, setIsEdittable] = useState(false);
 
     return (
         <div>
-        <button className ={`EditButton${isEdittable ? "Active" : ""}`}
-         onClick = {() => {setIsEdittable(!isEdittable)}}>EDIT</button>
-        <DesignBoard edit = {isEdittable}  />
-        <PadInput editMode = {isEdittable} />
+            {/** Edit Button **/}
+            <button className={`EditButton${isEdittable ? "Active" : ""}`}
+                onClick={() => { setIsEdittable(!isEdittable) }}>EDIT</button>
+            
+            {/** Reset Button **/}
+            <button className="ResetButton">RESET</button>
+
+            {/** Components **/}
+            <DesignBoard edit={isEdittable} />
+            <PadInput editMode={isEdittable} />
         </div>
     )
 }
--- src/components/DesignPad.js
+++ src/components/DesignPad.js
@@ -17,21 +17,32 @@ export default function DesignPad(props) {
 
         // Clear the drag data cache (for all formats/types)
         event.dataTransfer.clearData();
-       }
+    }
 
-       function allowDrop (event) {
+    function allowDrop(event) {
         event.preventDefault();
-     }
+    }
+
+    const resetDesignPad = () => {
+        const boxes = document.querySelectorAll(".DesignPad");
+        boxes.forEach((box) => {
+            box.style.backgroundColor = "";
+        })
+    }
 
     return (
         <div>
-        <h1 className = "DesignPad-h1">{props.editMode ? "DESIGN PAD (Edit Mode)" : "DESIGN PAD (View Mode)" }</h1>
+            <h1 className="DesignPad-h1">{props.editMode ? "DESIGN PAD (Edit Mode)" : "DESIGN PAD (View Mode)"}</h1>
             <div className="DesignPad-container">
-                <div id="designPad_box1" className="DesignPad-box1" style = {{backgroundColor:""}} onDrop={drop_handler} onDragOver={allowDrop}></div>
-                <div id="designPad_box2" className="DesignPad-box2" style = {{backgroundColor:""}} onDrop={drop_handler} onDragOver={allowDrop}></div>
-                <div id="designPad_box3" className="DesignPad-box3" style = {{backgroundColor:""}} onDrop={drop_handler} onDragOver={allowDrop}></div>
-                <div id="designPad_box4" className="DesignPad-box4" style = {{backgroundColor:""}} onDrop={drop_handler} onDragOver={allowDrop}></div>
+                <div id="designPad_box1" className="DesignPad" style={{ backgroundColor: "" }} onDrop={drop_handler} onDragOver={allowDrop}></div>
+                <div id="designPad_box2" className="DesignPad" style={{ backgroundColor: "" }} onDrop={drop_handler} onDragOver={allowDrop}></div>
+                <div id="designPad_box3" className="DesignPad" style={{ backgroundColor: "" }} onDrop={drop_handler} onDragOver={allowDrop}></div>
+                <div id="designPad_box4" className="DesignPad" style={{ backgroundColor: "" }} onDrop={drop_handler} onDragOver={allowDrop}></div>
             </div>
+
+            {/** Reset Button when clicked resets the design board **/}/
+            <button className="ResetButton" onClick={resetDesignPad}>RESET</button>
+
         </div>
     )
 }
