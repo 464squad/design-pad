e7d4be053158cb1dbc4f917e2549fdcb40c6891f
Ganesh Jainarain <RicheyJay2@gmail.com>
2020-11-10 23:43:17 -0500

    Worked on the DesignContainer and added state to the edit button, now there is a active and non active state for the edit button

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -1,64 +1,34 @@
 import React from 'react';
 import DesignBoard from './DesignBoard';
-import DesignPad from './DesignPad';
 import PadInput from './PadInput';
 
 class DesignContainer extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isBoxVisible: true
-          };
-    }
+  state = { EDIT: false };
 
-    toggleBox = () => {
-        this.setState(prevState => ({ isBoxVisible: !prevState.isBoxVisible }));
-    };
 
-    editButton = event => {
-        // to prevent page reload on form submit
-        // event.preventDefault();
-        /*
-            TODO - Logic for calling props to handle submission and setting state changes
-        */
-        // this.setState(true)
-        console.log('current state in edit button event',this.state)
-        // console.log('Im the edit button event')
-    }
+  Edit = () => {
+    this.setState(props => ({
+      editing: !props.editing
+    }));
+  };
+    
 
     render() {
-        const { isBoxVisible } = this.state;
-        
-        return ( 
-            <div className="container">
-
-                {/* Testing feature for edit button */}
-                <div className={`box ${isBoxVisible ? "" : "hidden"}`}>
-                    <p>{`${isBoxVisible ? "I'm the box" : "I'm the yellow box"}`}</p>
+        const button = this.state.EDIT ? "edit_button" : "edit_button_after";
+        const EDIT = this.state.EDIT
+        return (
+            <div>
+                <div>
+                    <h3>DESIGN PAD</h3>
+                    <button className={button} onClick={this.Edit}>
+                    EDIT
+                    </button>
                 </div>
-                
-                { /*TODO - Add editButton Component */ }
-                <div className="button_container">
-                    <button className={`edit_button ${isBoxVisible ? "" : "hidden"}`} onClick={this.toggleBox}> Edit </button>
-                </div>
-                
-                <br/> 
 
-                { /*TODO - Add PadInput Component */ } 
-                <PadInput/>
-
-                <br/> 
-                
-                { /*TODO - Add DesignPad Component */ } 
-                <DesignPad/>
-
-                <br/> 
-
-                { /*TODO - Add DesignBoard Component */ } 
-                <DesignBoard/>
-
+                <PadInput EDIT={EDIT} />
+                <DesignBoard />
             </div>
-        );
+    );
     }
 }
 
--- src/components/DesignPad.js
+++ src/components/DesignPad.js
@@ -1,32 +1,5 @@
 import React from 'react';
 
-const DesignPadBody = () => {
-    // boilerplate table header functional component
-    return ( 
-
-        <div>
-            <div class="pad_container">
-                <div className="pads" id="pads1" 
-                style={{ backgroundColor: "yellow" }} draggable={true} > 1 </div>
-                <div className="pads" id="pads2"
-                style={{ backgroundColor: "orange" }} draggable={true} > 2 </div>
-                <div className="pads" id="pads3"
-                style={{ backgroundColor: "red" }} draggable={true} > 3 </div>
-                <div className="pads" id="pads4"
-                style={{ backgroundColor: "purple" }} draggable={true} > 4 </div>
-                <div className="pads" id="pads5"
-                style={{ backgroundColor: "blueviolet" }} draggable={true} > 5 </div>
-                <div className="pads" id="pads6"
-                style={{ backgroundColor: "turquoise" }} draggable={true} > 6 </div>
-                <div className="pads" id="pads7"
-                style={{ backgroundColor: "green" }} draggable={true} > 7 </div>
-                <div className="pads" id="pads8"
-                style={{ backgroundColor: "brown" }} draggable={true} > 8 </div>
-            </div>
-        </div>
-        
-    );
-}
 
 const DragAndDrop = props => {
     const handleDragEnter = e => {
@@ -45,16 +18,7 @@ const DragAndDrop = props => {
       e.preventDefault();
       e.stopPropagation();
     };
-    return (
-      <div className={'drag-drop-zone'}
-        onDrop={e => handleDrop(e)}
-        onDragOver={e => handleDragOver(e)}
-        onDragEnter={e => handleDragEnter(e)}
-        onDragLeave={e => handleDragLeave(e)} >
-            
-        <p>Drag files here to upload</p>
-      </div>
-    );
+    
   };
 
 
