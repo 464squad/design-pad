f66a45b1ed7463a518c54a9b4290291c96a39ee5
hukushpakush <sharmin.tamanna@lc.cuny.edu>
2020-12-21 23:41:27 -0500

    added api using hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React , {useState , useEffect} from "react";
 
 function Home() {
-    return <h2>Home</h2>;
-  }
+    const [joke, setJoke] = useState({setup:"", punchline: ""});
+    useEffect(() => {
+        const fetchJokeJSON = async() => {
+            const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+            const joke = await response.json();
+            console.log(joke)
+            setJoke(joke)
+          };
+            fetchJokeJSON()
+    },[]);
+    return (
+        <div>
+         <h2>Home</h2>
+    <p>{joke.setup}</p>
+    <p>{joke.punchline}</p>
+        </div>
+    )}
 
   export default Home
