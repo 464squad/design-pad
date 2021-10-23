3f0c3aed8ef10cf69a73021646ef58f277bb4ebf
Cchanyu <Chanyu96@gmail.com>
2021-10-23 15:52:14 -0400

    boilerplate set up

---
+++ src/components/DesignContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class DesignContainer extends React.Component {
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default DesignContainer;
---
+++ src/components/PadInput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class PadInput extends React.Component {
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default PadInput;
