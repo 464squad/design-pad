baf714333572c9493b9c743a7c37048e84750ee2
Lessie Quezada <Lessieq07@gmail.com>
2021-10-24 13:20:17 -0400

    Aligned the boxes in the center, fixed the edit button position

--- src/components/DesignPad.js
+++ src/components/DesignPad.js
@@ -7,12 +7,16 @@ function DesignPad() {
     //need to create a drop to transferdata to the pad link if you need help https://www.pluralsight.com/guides/drag-and-drop-react-components  
     // https://www.youtube.com/watch?v=-MfTv5VRM0A https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
     return (
-        <div className='right-box'>
-            <div className='pad' 
+        <div className='row right-box'>
+            <div className='col pad' 
             style={{backgroundColor: 'lightgrey'}}
             
             >
+            <div id='footer'>
+
             </div>
+            </div>
+            
 
         </div>
 
