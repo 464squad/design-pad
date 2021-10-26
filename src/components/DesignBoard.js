83fccb2510c9d4d69d019201363f60ca6e649b30
Gainz90 <76008691+Gainz90@users.noreply.github.com>
2021-10-26 15:40:00 -0400

    Squares made

---
+++ src/components/DesignBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import DesignPad from './components/DesignPad'
+
+class DesignBoard extends React.Component {
+    render() { 
+        return <div>
+            <div className="design-board1"></div>
+            <div className="design-board2"></div>
+            <div className="design-board3"></div>
+            <div className="design-board4"></div>
+        </div>;
+    }
+}
+ 
+export default DesignBoard;
