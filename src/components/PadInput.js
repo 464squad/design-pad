761914456196d35a79aa4c8a109f9d350f9074db
EricBuddy <Eric.Buddy00@gmail.com>
2021-10-25 21:23:46 -0400

    change brach

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './PadInput.css'
 function PadInput(props) {
-  
+    const dragStart = event =>{
+        
+        event.dataTransfer.setData('colors', 'black')
+       
+    }
+    const dragOver=event=>{
+        event.preventDefault();
+    }
      // you need to create a drag start for each button  and dragover
    // if you need help https://www.pluralsight.com/guides/drag-and-drop-react-components  
     // https://www.youtube.com/watch?v=-MfTv5VRM0A https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
     return (
         <div className='left-box'>
            <div className='inner-box'>
-            <button draggable='true'>1</button>
+            <button draggable='true' onDragStart={dragStart}onDragOver={dragOver}>1</button>
             <button draggable='true'>2</button>
             <button draggable='true'>3</button>
             <button draggable='true'>4</button>
