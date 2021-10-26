3234736ae4615fad6b08395e3b26d8adaf092789
Desiree Burrell <desireeburrell333@desirees-air.myfiosgateway.com>
2021-10-26 19:33:50 -0400

    bareback template

--- src/Componets/PadInput.js
+++ src/Componets/PadInput.js
@@ -9,14 +9,54 @@ export class PadInput extends Component {
 
 
 
-               <div style={{width:'50px',borderRadius:'15px',backgroundColor:'lightgray',}}draggable='true'> 1</div> 
-               <div style={{width:'50px',borderRadius:'15px',backgroundColor:'lightgray',}}draggable='true'> 2</div> 
-               <div style={{width:'50px',borderRadius:'15px',backgroundColor:'lightgray',}}draggable='true'> 3</div> 
-               <div style={{width:'50px',borderRadius:'15px',backgroundColor:'lightgray',}}draggable='true'> 4</div> 
-               <div style={{width:'50px',borderRadius:'15px',backgroundColor:'lightgray',}}draggable='true'> 5</div> 
-               <div style={{width:'50px',borderRadius:'15px',backgroundColor:'lightgray',}}draggable='true'> 6</div> 
-               <div style={{width:'50px',borderRadius:'15px',borderRadius:'15px',backgroundColor:'lightgray',}}draggable='true'> 7</div> 
-               <div style={{width:'50px',borderRadius:'15px',backgroundColor:'lightgray',}}draggable='true'> 8</div> 
+               <div style={{width:'50px',
+               borderRadius:'15px',
+               backgroundColor:'lightgray',}}
+               draggable='true'>
+                    1
+                    </div> 
+               <div style={{width:'50px',
+               borderRadius:'15px',
+               backgroundColor:'lightgray',}}
+               draggable='true'>
+                    2
+                    </div> 
+               <div style={{width:'50px',
+               borderRadius:'15px',
+               backgroundColor:'lightgray',}}
+               draggable='true'>
+                    3
+                    </div> 
+               <div style={{width:'50px',
+               borderRadius:'15px',
+               backgroundColor:'lightgray',}}
+               draggable='true'>
+                    4
+                    </div> 
+               <div style={{width:'50px',
+               borderRadius:'15px',
+               backgroundColor:'lightgray',}}
+               draggable='true'>
+                    5
+                    </div> 
+               <div style={{width:'50px',
+               borderRadius:'15px',
+               backgroundColor:'lightgray',}}
+               draggable='true'>
+                    6
+                    </div> 
+               <div style={{width:'50px',
+               borderRadius:'15px',
+               borderRadius:'15px',background
+               Color:'lightgray'
+               ,}
+               }draggable='true'> 7</div> 
+               <div style={{width:'50px',
+               borderRadius:'15px',
+               backgroundColor:'lightgray',}}
+               draggable='true'>
+                    8
+                    </div> 
                 
                
                
