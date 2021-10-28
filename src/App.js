1ffe197b42c8b821fe18208b54e513ab74efae53
Josseline <guardadojosseline@gmail.com>
2021-10-27 20:05:01 -0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PadInput from './Components/PadInput';
+import DesignBoard from './Components/DesignBoard';
+import DesignPad from './Components/DesignPad';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+       <PadInput/>
+       <DesignBoard />
+       <DesignPad/>
+      
+      </div>
     </div>
   );
 }
