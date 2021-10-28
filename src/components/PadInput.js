cd0605e7cb6a46a6a1c61bd60e3e476cfff7af87
ATEOTG <bryansantos9797@gmail.com>
2021-10-27 22:36:05 -0400

    added drag functionality to the padInput

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -9,11 +9,41 @@ class PadInput extends React.Component {
 
     }
 
-    
+
+    //gives us the value of the key that is currently being dragged
+    dragStart = (event) => {
+        const target = event.target.id
+        event.dataTransfer.setData("keypad_id", target);
+        console.log(target)
+        /*
+        return {
+            
+            this.props.handleKey(target)
+        }
+        */
+        
+    } 
+
+    //Drop function may need to exist outside this component
+    /*
+    drop = (event) => {
+        event.preventDefault();
+        var keypad_id = event.dataTransfer.getData("keypad_id");
+        console.log(keypad_id);
+
+    } */
+
+
+    dragOver = (event) => {
+        console.log("Dragover");
+        event.preventDefault();
+    }
 
     render() {
-        return <div class = "flex-container" >
-            <div id = "k1" draggable = "true" > 1 </div>
+        // creates boxes and makes them draggable 
+        return <div className = "flex-container" onDrop = {this.drop} onDragStart = {this.dragStart} 
+                 onDragOver = {this.dragOver}>
+            <div id = "k1" draggable = "true"  > 1 </div>
             <div id = "k2" draggable = "true"> 2 </div>
             <div id = "k3" draggable = "true"> 3 </div>
             <div id = "k4" draggable = "true"> 4 </div>
