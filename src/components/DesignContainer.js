afb0898fe240554b9e49718eaee85287e737d269
Mariano Moran <marianomoran@Marianos-MBP.home>
2021-10-29 19:23:10 -0400

    on new branch

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -3,6 +3,7 @@ import DesignBoard from "./DesignBoard";
 import PadInput from "./PadInput";
 
 function DesignContainer(props) {
+
   //Implement PadInput and grab the 8 inputs, pass props in editMode(false) and setCurrentDragged 
   //Creating an EDIT button and determining its state with a click on the button 
   return (
@@ -13,7 +14,7 @@ function DesignContainer(props) {
         setCurrentDragged={props.setCurrentDragged}
       />
       <button
-        className={`edit-button ${props.editMode ? "" : "edit-mode"}`} 
+        className={`edit-button ${props.editMode ? "" : "disabled"}`} 
         onClick={() => props.setEditMode(true)}>EDIT</button>
 
       <DesignBoard currentDragged={props.currentDragged} />
