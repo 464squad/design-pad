7ec0cb4623fb3b46db05f3931330a9eb2147bf11
Josseline <guardadojosseline@gmail.com>
2021-10-29 17:29:45 -0400

    Revert "commit":
    This reverts commit 1de6d0b7182e2d353400bfdaa5c9d39ffda03111.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, {useState} from 'react';
 import './App.css';
 // components
 import DesignBoard from "./components/DesignBoard";
-import CHOICE from "./components/CHOICE";
-import EDIT from './components/EDIT';
-import PadInput from './components/PadInput/PadInput'
 
 function App() {
   
@@ -12,10 +9,7 @@ function App() {
   return (
     <div className="App">
       <DesignBoard />
-      <EDIT />
-      <CHOICE />
-      <PadInput/>
-        </div>
+    </div>
   );
 }
 
