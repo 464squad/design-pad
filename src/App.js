4dc74250ec8722325450a1ea9f9caa06ff9feb87
Josseline <guardadojosseline@gmail.com>
2021-10-30 23:50:53 -0400

    Resolved Merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 // components
+import DesignBoard from "./components/DesignBoard";
+import CHOICE from "./components/CHOICE";
+import EDIT from './components/EDIT';
 import PadInput from './components/PadInput/PadInput'
 
 function App() {
@@ -8,9 +11,23 @@ function App() {
   
   return (
     <div className="App">
+      <DesignBoard />
+      <EDIT />
+      <CHOICE />
       <PadInput/>
         </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
