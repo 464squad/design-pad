21aa318b2f16258dceabe06f015d85f9470b24bb
Mr-Smithy-x <CharltonSmith@outlook.com>
2021-11-01 16:03:19 -0400

    added drag event

--- src/Componets/PadInput.js
+++ src/Componets/PadInput.js
@@ -8,17 +8,22 @@ export class PadInput extends Component {
      borderRadius:'15px',
      backgroundColor:'lightgray',}
 
+
+     handleDrag = (event) => {
+          console.log(event)
+     }
+
     render() {
         return (
             <div className="col-12" style={{backgroundColor:'blueviolet'}}>
-               <Button draggable='true'> 1 </Button>
-               <Button draggable='true'> 2 </Button>
-               <Button draggable='true'> 3 </Button>
-               <Button draggable='true'> 4 </Button>
-               <Button draggable='true'> 5 </Button>
-               <Button draggable='true'> 6 </Button>
-               <Button draggable='true'> 7 </Button>
-               <Button draggable='true'> 8 </Button>
+               <Button onDragEnd={this.handleDrag} draggable='true'> 1 </Button>
+               <Button onDragEnd={this.handleDrag} draggable='true'> 2 </Button>
+               <Button onDragEnd={this.handleDrag} draggable='true'> 3 </Button>
+               <Button onDragEnd={this.handleDrag} draggable='true'> 4 </Button>
+               <Button onDragEnd={this.handleDrag} draggable='true'> 5 </Button>
+               <Button onDragEnd={this.handleDrag} draggable='true'> 6 </Button>
+               <Button onDragEnd={this.handleDrag} draggable='true'> 7 </Button>
+               <Button onDragEnd={this.handleDrag} draggable='true'> 8 </Button>
             </div>
         )
     }
