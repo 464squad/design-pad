946fd48610cfe1cecb6ee1520acb30b824322769
Michelle Otero <michelle.otero@lc.cuny.edu>
2021-11-01 20:41:52 -0400

    first design pad attempt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import DesignContainer from './components/DesignContainer'
 import './App.css';
+import DesignPad from './components/DesignPad';
 
 function App() {
   return (
     <div className="App">
       <DesignContainer />
+      <DesignPad/>
+      
+
+      
     </div>
   );
 }
--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+
 
 class PadInput extends React.Component {
-
+     
     render(){
         return(
             <div></div>
         )
-    }
+    
+        }
 }
 
 export default PadInput;
