87686150f85031ea108a5ee8b429bb3159b6c4e2
Mr-Smithy-x <CharltonSmith@outlook.com>
2021-11-01 20:50:24 -0400

    testing events

--- src/Componets/DesignPad.js
+++ src/Componets/DesignPad.js
@@ -6,13 +6,41 @@ export class DesignPad extends Component {
         
        this.state = {
            color: 'grey'
-
        }
+    }
+
+    onDrop = (event) =>  {
+        const droppedItem = event.nativeEvent;
+        event.preventDefault()
+        console.log("Dropped")
+        //console.log(event)
+        console.log(droppedItem)
+        console.log(droppedItem)
+    }
+
+    onDragEnter = event => {
+        event.preventDefault()
+        // Bring the endzone back to normal, maybe?
+    };
+
+    onDragLeave = event => {
+        event.preventDefault()
+        // Bring the endzone back to normal, maybe?
+    };
+
+    onDragOver = (event) => {
+        event.preventDefault()
+        //console.log("Over")
     }
 
     render() {
         return (
-            <div className="p-3 border bg-dark"  
+            <div 
+            onDrop={this.onDrop} 
+            onDragOver={this.onDragOver} 
+            onDragEnter={this.onDragEnter}
+            onDragLeave={this.onDragLeave}
+            className="p-3 border bg-dark"  
             style={{ height: '300px', width: '300px',
             margin: '10px',
             borderRadius: "25px",
--- src/Componets/PadInput.js
+++ src/Componets/PadInput.js
@@ -16,14 +16,14 @@ export class PadInput extends Component {
     render() {
         return (
             <div className="col-12" style={{backgroundColor:'blueviolet'}}>
-               <Button onDragEnd={this.handleDrag} draggable='true'> 1 </Button>
-               <Button onDragEnd={this.handleDrag} draggable='true'> 2 </Button>
-               <Button onDragEnd={this.handleDrag} draggable='true'> 3 </Button>
-               <Button onDragEnd={this.handleDrag} draggable='true'> 4 </Button>
-               <Button onDragEnd={this.handleDrag} draggable='true'> 5 </Button>
-               <Button onDragEnd={this.handleDrag} draggable='true'> 6 </Button>
-               <Button onDragEnd={this.handleDrag} draggable='true'> 7 </Button>
-               <Button onDragEnd={this.handleDrag} draggable='true'> 8 </Button>
+               <Button color="lol1" onDragEnd={this.handleDrag} draggable='true'> 1 </Button>
+               <Button color="lol2" onDragEnd={this.handleDrag} draggable='true'> 2 </Button>
+               <Button color="lol3" onDragEnd={this.handleDrag} draggable='true'> 3 </Button>
+               <Button color="lol4" onDragEnd={this.handleDrag} draggable='true'> 4 </Button>
+               <Button color="lol5" onDragEnd={this.handleDrag} draggable='true'> 5 </Button>
+               <Button color="lol6" onDragEnd={this.handleDrag} draggable='true'> 6 </Button>
+               <Button color="lol7" onDragEnd={this.handleDrag} draggable='true'> 7 </Button>
+               <Button color="lol8" onDragEnd={this.handleDrag} draggable='true'> 8 </Button>
             </div>
         )
     }
